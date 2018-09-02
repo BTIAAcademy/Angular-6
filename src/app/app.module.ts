f5e3a86dbcbc1db66b52cbd405bd6ca3d7d80e75
KarimGitHub <karim.fan4@gmail.com>
2018-09-02 14:10:04 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchFilterPipe } from './pipe/search-filter.pipe';
+import { SearchListComponent } from './list/search-list/search-list.component';
+import { InputTextComponent } from './header/input-text/input-text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFilterPipe,
+    SearchListComponent,
+    InputTextComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
